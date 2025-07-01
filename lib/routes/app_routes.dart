552346bcb36695291c4318b9b9fd9f8e903eb3d8
prince <princesuke@gmail.com>
2025-07-01 10:58:21 +0700

    feat: add gen qr routes

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -1,15 +1,18 @@
 import '../screens/home_page.dart';
 import '../screens/qr_scan_page.dart';
 import '../screens/scanned_result_page.dart';
+import '../screens/gen_qr_page.dart';
 
 class AppRoutes {
   static const String home = '/home';
   static const String qrScan = 'qr_scan';
   static const String scannedResult = '/scanned_result';
+  static const String genQr = '/gen_qr';
 
   static final routes = {
     home: (context) => HomePage(),
     qrScan: (context) => QrScanPage(),
     scannedResult: (context) => ScannedResultPage(),
+    genQr: (context) => GenQrPage(),
   };
 }
