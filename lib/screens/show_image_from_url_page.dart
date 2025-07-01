e1e5ff44505cea05210e386b41215a49b642557d
prince <princesuke@gmail.com>
2025-07-01 11:34:23 +0700

    feat: add logic show image url

--- lib/screens/show_image_from_url_page.dart
+++ lib/screens/show_image_from_url_page.dart
@@ -5,9 +5,27 @@ class ShowImageFromUrlPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final args =
+        ModalRoute.of(context)!.settings.arguments as Map<String, dynamic>?;
+    final String? url = args?['url'] as String?;
+
     return Scaffold(
       appBar: AppBar(title: const Text('แสดงรูปจาก QR Code')),
-      body: Column(children: []),
+      body: Column(
+        children: [
+          Image.network(
+            url!,
+            errorBuilder: (_, __, ___) => const Text('โหลดรูปไม่ได้'),
+          ),
+          const SizedBox(height: 20),
+          SelectableText(url, textAlign: TextAlign.center),
+          const SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: () => Navigator.popUntil(context, (r) => r.isFirst),
+            child: const Text('กลับหน้าแรก'),
+          ),
+        ],
+      ),
     );
   }
 }
