e586d11076952c41739860f5b7d4e6e958aaa797
prince <princesuke@gmail.com>
2025-07-01 10:38:30 +0700

    feat: update logic result page

--- lib/screens/scanned_result_page.dart
+++ lib/screens/scanned_result_page.dart
@@ -5,9 +5,31 @@ class ScannedResultPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final args =
+        ModalRoute.of(context)!.settings.arguments as Map<String, dynamic>?;
+    final String? result = args?['result'] as String?;
+
     return Scaffold(
       appBar: AppBar(title: const Text('ผลลัพธ์ QR Code')),
-      body: Text('qr code'),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Icon(Icons.check_circle, color: Colors.green, size: 80),
+            const SizedBox(height: 20),
+            Text(
+              result!,
+              textAlign: TextAlign.center,
+              style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 30),
+            ElevatedButton(
+              onPressed: () => Navigator.popUntil(context, (r) => r.isFirst),
+              child: const Text('กลับหน้าแรก'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
