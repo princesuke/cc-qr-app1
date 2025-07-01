12bdde24407c62df1f2e09fa3380357989324f01
prince <princesuke@gmail.com>
2025-07-01 11:20:11 +0700

    feat: mapping show image routes

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -2,17 +2,20 @@ import '../screens/home_page.dart';
 import '../screens/qr_scan_page.dart';
 import '../screens/scanned_result_page.dart';
 import '../screens/gen_qr_page.dart';
+import '../screens/show_image_from_url_page.dart';
 
 class AppRoutes {
   static const String home = '/home';
   static const String qrScan = 'qr_scan';
   static const String scannedResult = '/scanned_result';
   static const String genQr = '/gen_qr';
+  static const String showImageFromUrl = '/show_image_from_url';
 
   static final routes = {
     home: (context) => HomePage(),
     qrScan: (context) => QrScanPage(),
     scannedResult: (context) => ScannedResultPage(),
     genQr: (context) => GenQrPage(),
+    showImageFromUrl: (context) => ShowImageFromUrlPage(),
   };
 }
